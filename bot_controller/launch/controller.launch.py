915581e370c1b2946fb28befd717d46df87664ef
pheonix-19 <am2836166@gmail.com>
2024-12-29 20:18:20 +0530

    added the difrrential drive controller

--- bot_controller/launch/controller.launch.py
+++ bot_controller/launch/controller.launch.py
@@ -5,6 +5,10 @@ from launch.substitutions import LaunchConfiguration
 from launch.conditions import UnlessCondition, IfCondition
 
 def generate_launch_description():
+    use_sim_time_arg = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="True",
+    )
     use_python_arg = DeclareLaunchArgument(
         "use_python",
         default_value="False",
@@ -17,10 +21,26 @@ def generate_launch_description():
         "wheel_separation",
         default_value="0.17",
     )
+    use_simple_controller_arg = DeclareLaunchArgument(
+        "use_simple_controller",
+        default_value="True",
+    )
 
+    use_sim_time = LaunchConfiguration("use_sim_time")
     use_python = LaunchConfiguration("use_python")
     wheel_radius = LaunchConfiguration("wheel_radius")
     wheel_separation = LaunchConfiguration("wheel_separation")
+    use_simple_controller = LaunchConfiguration("use_simple_controller")
+
+    wheel_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["bot_controller", 
+                   "--controller-manager", 
+                   "/controller_manager"
+        ],
+        condition=UnlessCondition(use_simple_controller),
+    )
 
     
 
@@ -33,21 +53,28 @@ def generate_launch_description():
             "/controller_manager",
         ]
     )
-    simple_controller = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[
-            "simple_velocity_controller",
-            "--controller-manager",
-            "/controller_manager",
+    simple_controller = GroupAction(
+        condition=IfCondition(use_simple_controller),
+        actions=[
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=["simple_velocity_controller", 
+                        "--controller-manager", 
+                        "/controller_manager"
+                ]
+            ),
+            Node(
+                package="bot_controller",
+                executable="simple_controller.py",
+                parameters=[
+                    {"wheel_radius": wheel_radius,
+                    "wheel_separation": wheel_separation,
+                    "use_sim_time": use_sim_time}],
+                condition=IfCondition(use_python),
+            )
+            
         ]
-    )
-
-    simple_controller_py = Node(
-        package="bot_controller",
-        executable="simple_controller.py",
-        parameters=[{"wheel_radius": wheel_radius,
-                      "wheel_separation": wheel_separation}]
     )
 
 
@@ -56,8 +83,10 @@ def generate_launch_description():
             use_python_arg,
             wheel_radius_arg,
             wheel_separation_arg,
+            use_simple_controller_arg,
+            wheel_controller_spawner,
             joint_state_broadcaster_spawner,
-            simple_controller,
-            simple_controller_py        
+            simple_controller
+                   
             ]
     )
